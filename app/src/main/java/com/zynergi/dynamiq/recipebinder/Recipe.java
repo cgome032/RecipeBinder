e15a1cd4377b8170196952caffd431120658d71d
Carlos Gomez <cgome032@ucr.edu>
2018-10-16 21:23:35 -0700

    Created Recipe class
    Created the recipe class inside of the package, com.zynergi.dynamiq.recipebinder that will contain a ArrayList of Strings to hold the ingredients.

---
+++ app/src/main/java/com/zynergi/dynamiq/recipebinder/Recipe.java
@@ -0,0 +1,26 @@
+package com.zynergi.dynamiq.recipebinder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recipe {
+
+    // Ingredients list for Recipe
+    private List<String> ingredients;
+
+    public Recipe() {
+        this.ingredients = new ArrayList<>();
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void addIngredient(String ingredient) {
+        this.ingredients.add(ingredient);
+    }
+}

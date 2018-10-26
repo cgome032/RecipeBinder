bf5deab830afd4430671ae7092116afd39d19be7
Kau Chung <kchun014@ucr.edu>
2018-10-26 13:27:23 -0700

    Adding Recipe UI skeleton (still need to flesh out actual recipe retrieval from firebase)

---
+++ app/src/main/java/com/zynergi/dynamiq/recipebinder/Recipe_Activity.java
@@ -0,0 +1,19 @@
+package com.zynergi.dynamiq.recipebinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recipe_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_);
+    }
+
+    //Recipe ID grab data
+    //Recipe name
+    //Recipe Ingredient List
+    //Recipe Instructions List
+
+}

a51e89d4d12b8f967fd262bbc8c772dbd3a1a84e
Parsakon <phomv001@ucr.edu>
2018-10-29 17:47:11 -0700

    sending to realtime DB works for recipe

--- app/src/main/java/com/zynergi/dynamiq/recipebinder/createRecipeActivity.java
+++ app/src/main/java/com/zynergi/dynamiq/recipebinder/createRecipeActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.FirebaseError;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.zynergi.dynamiq.recipebinder.Post.Recipe;
@@ -16,16 +19,15 @@ public class createRecipeActivity extends AppCompatActivity {
 
     Recipe completedRecipe = new Recipe();
     ArrayList<String> steps;
-
-    private DatabaseReference mDatabase;
-
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference("recipes");
         setContentView(R.layout.activity_create_recipe);
         steps = new ArrayList<>();
+
 
     }
 
@@ -33,7 +35,7 @@ public class createRecipeActivity extends AppCompatActivity {
         EditText eIngredients = (EditText) findViewById(R.id.editIngredients);
         String tmpIngredients = eIngredients.getText().toString();
         completedRecipe.addIngredient(tmpIngredients);
-        eIngredients.setText("");
+        eIngredients.getText().clear();
 
     }
 
@@ -41,7 +43,8 @@ public class createRecipeActivity extends AppCompatActivity {
         EditText eSteps = findViewById(R.id.editSteps);
         String tmpSteps = eSteps.getText().toString();
         steps.add(tmpSteps);
-        eSteps.setText("");
+        eSteps.getText().clear();
+
     }
 
     public void submitRecipe(View view){
@@ -49,9 +52,13 @@ public class createRecipeActivity extends AppCompatActivity {
         String sName = eName.getText().toString();
         completedRecipe.setName(sName);
         completedRecipe.setSteps(steps);
+        String id = mDatabase.push().getKey();
 
-        mDatabase.child("Recipes").setValue(completedRecipe);
-
-
+        mDatabase.child(id).setValue(completedRecipe, new DatabaseReference.CompletionListener(){
+            public void onComplete(DatabaseError error, DatabaseReference ref){
+                System.out.println("Value was set. Error = "+error);
+                Toast.makeText(createRecipeActivity.this,"word",Toast.LENGTH_SHORT);
+                }
+        });
     }
 }

d3f6d8e9640713e47ea5667dff183f90bfd9470a
Carlos Gomez <cgome032@ucr.edu>
2018-10-29 21:33:55 -0700

    Updated Profile class file
    Added Lists for both favorite recipes and a profile's personal recipes.

--- app/src/main/java/com/zynergi/dynamiq/recipebinder/Profile/Profile.java
+++ app/src/main/java/com/zynergi/dynamiq/recipebinder/Profile/Profile.java
@@ -1,15 +1,31 @@
 package com.zynergi.dynamiq.recipebinder.Profile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Profile {
     // Profile name
     private String name;
     private String description;
-
+    private List<String> recipeNames;
+    private List<String> favoriteRecipes;
 
     public Profile() {
         this.name = "";
         this.description = "";
+        this.recipeNames = new ArrayList<>();
+        this.favoriteRecipes = new ArrayList<>();
     }
+
+    public List<String> getRecipeNames() {
+        return recipeNames;
+    }
+
+    public void setRecipeNames(List<String> recipeNames) { this.recipeNames = recipeNames; }
+
+    public List<String> getFavoriteRecipes() { return favoriteRecipes; }
+
+    public void setFavoriteRecipes(List<String> favoriteRecipes) { this.favoriteRecipes = favoriteRecipes; }
 
     public String getName() {
         return name;
@@ -26,4 +42,8 @@ public class Profile {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public void addRecipeName(String recipeName) { this.recipeNames.add(recipeName); }
+
+    public void addFavoriteRecipe(String favoriteName) { this.favoriteRecipes.add(favoriteName); }
 }

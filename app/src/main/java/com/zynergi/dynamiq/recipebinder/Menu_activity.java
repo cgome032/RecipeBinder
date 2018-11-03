e3e3812ce1173c6fdbb0359ffb1f17e50ac1a831
Carlos Gomez <cgome032@ucr.edu>
2018-11-03 00:57:05 -0700

    Added BottomNavigationView to allow for easier navigation of menu screen.
    BottomNavigationView has been added to the main menu activity and allows for easier navigation.  The fragments corresponding to each menu have not been created and will be setup for the feed along with a tutorial to allow other users to setup faster.

--- app/src/main/java/com/zynergi/dynamiq/recipebinder/Menu_activity.java
+++ app/src/main/java/com/zynergi/dynamiq/recipebinder/Menu_activity.java
@@ -1,9 +1,14 @@
 package com.zynergi.dynamiq.recipebinder;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
+import android.support.design.widget.BottomNavigationView;
+import android.widget.Toast;
 
 public class Menu_activity extends AppCompatActivity {
 
@@ -22,4 +27,49 @@ public class Menu_activity extends AppCompatActivity {
         Intent myIntent = new Intent(this, Recipe_Activity.class);
         startActivity(myIntent);
     }
+
+    private void setBottomNavigation() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
+        //bottomNavigationView.setSelectedItemId(R.id.home_screen_fragment);
+
+        bottomNavigationView.getMenu().findItem(R.id.action_home).setChecked(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        //fragment = new HomeScreenFragment();
+                        //loadFragment(fragment);
+
+                        break;
+
+                    case R.id.action_favorites:
+                        Toast.makeText(Menu_activity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_create_recipe:
+                        Toast.makeText(Menu_activity.this, "Create Recipe", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.action_account:
+                        Toast.makeText(Menu_activity.this, "Account", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+
+    /*
+    private void loadFragment(Fragment fragment) {
+        // Loading fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+    */
 }

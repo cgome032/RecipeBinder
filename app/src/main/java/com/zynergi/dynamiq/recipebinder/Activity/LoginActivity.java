cc0ddc19152871ee94a44ead01b776f0e1dfefeb
Carlos Gomez <cgome032@ucr.edu>
2018-11-10 09:39:50 -0800

    Create Recipe Activity fix now enables recipes to be loaded into database
    Fragment works correctly by adding OnClickListener to onCreateView method and calling the appropriate methods.  There is an issue still with creating the recipes that save the prior ingredients instead of starting a new recipe object.

--- app/src/main/java/com/zynergi/dynamiq/recipebinder/Activity/LoginActivity.java
+++ app/src/main/java/com/zynergi/dynamiq/recipebinder/Activity/LoginActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.zynergi.dynamiq.recipebinder.Menu_activity;
 import com.zynergi.dynamiq.recipebinder.R;
 
 public class LoginActivity extends AppCompatActivity {

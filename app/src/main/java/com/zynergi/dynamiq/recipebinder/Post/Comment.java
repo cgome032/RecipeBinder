ce090d5d1970930a68175dcfa128f0d1a8c22bc7
Jake <jkuzn001@ucr.edu>
2018-11-13 15:41:49 -0800

    fixing issues with spacing in post activity

--- app/src/main/java/com/zynergi/dynamiq/recipebinder/Post/Comment.java
+++ app/src/main/java/com/zynergi/dynamiq/recipebinder/Post/Comment.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 public class Comment implements Serializable {
     private String comment;
 
-    public Comment(String comment) {
+    public Comment(String  comment) {
         this.comment = comment;
     }
 

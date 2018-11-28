aa874858bb88948728e15b35313c059f18839e3d
Jake <jkuzn001@ucr.edu>
2018-11-27 20:42:35 -0800

    Feed works with new changes

--- app/src/main/java/com/zynergi/dynamiq/recipebinder/Adapter/PostAdapter.java
+++ app/src/main/java/com/zynergi/dynamiq/recipebinder/Adapter/PostAdapter.java
@@ -1,14 +1,20 @@
 package com.zynergi.dynamiq.recipebinder.Adapter;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.view.menu.MenuView;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.zynergi.dynamiq.recipebinder.Activity.PostActivity;
 import com.zynergi.dynamiq.recipebinder.Post.Post;
 import com.zynergi.dynamiq.recipebinder.R;
 
@@ -18,24 +24,42 @@ public class PostAdapter extends FirestoreRecyclerAdapter<Post, PostAdapter.Post
     }
 
     @Override
-    protected void onBindViewHolder( @NonNull PostHolder holder, int position, @NonNull Post model) {
+    protected void onBindViewHolder(@NonNull final PostHolder holder, int position, @NonNull final Post model) {
         holder.recipeName.setText(model.getRecipe().getName());
+
+        holder.recipeCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(holder.Parent.getContext(), PostActivity.class);
+                intent.putExtra("Post", model);
+                intent.putExtra("RecipeName", model.getRecipe().getName());
+                intent.putExtra("RecipeId", model.getRecipeId());
+                //Log.d("FeedAdapter", "Recipe Id " + mData.get(position).getRecipeId());
+                holder.Parent.getContext().startActivity(intent);
+            }
+        });
+
     }
 
     @NonNull
     @Override
     public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_post_view, parent, false);
-        return new PostHolder(v);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.feedcard, parent, false);
+        return new PostHolder(v, parent);
     }
 
     public static class PostHolder extends RecyclerView.ViewHolder {
-        private Button recipeName;
+        private TextView recipeName;
+        private CardView recipeCard;
+        public ViewGroup Parent;
 
-        public PostHolder(View itemView) {
+        public PostHolder(View itemView, ViewGroup parent) {
             super(itemView);
 
-            recipeName = itemView.findViewById(R.id.button);
+            Parent = parent;
+            recipeName = itemView.findViewById(R.id.cardText);
+            recipeCard = itemView.findViewById(R.id.postCard);
         }
     }
 }

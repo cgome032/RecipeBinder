5b8b61c7c217e9366b62b6778a18940cc69a797b
Parsakon <phomv001@ucr.edu>
2018-11-27 21:38:38 -0800

    2 files

--- app/src/main/java/com/zynergi/dynamiq/recipebinder/ProfileActivity.java
+++ app/src/main/java/com/zynergi/dynamiq/recipebinder/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.zynergi.dynamiq.recipebinder;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -12,11 +13,15 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,6 +29,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.zynergi.dynamiq.recipebinder.Activity.LoginActivity;
 import com.zynergi.dynamiq.recipebinder.Adapter.recipesAdapter;
 import com.zynergi.dynamiq.recipebinder.Post.MyAdapter;
 import com.zynergi.dynamiq.recipebinder.Profile.Profile;
@@ -34,22 +40,26 @@ import java.util.List;
 public class ProfileActivity extends Fragment {
 
     private static TextView txtProfileName;
-    private static TextView txtDescription;
-
+    private static EditText txtDescription;
+    private String id;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth mAuth;
     private Profile mUser;
     private static final String TAG = "found user at FS";
-
+    private Button btnDescr;
+    private Button btnSignUp;
+    /*
     private RecyclerView mRecyclerViewRecipes;
     private recipesAdapter AdapterRecipes;
     private RecyclerView.LayoutManager mLayoutManagerRecipes;
-    private Context mContext;
     private List<String> recipeList;
+    */
+    private Context mContext;
+
     View rootView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String id;
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         mContext = getActivity().getApplicationContext();
 
@@ -77,6 +87,7 @@ public class ProfileActivity extends Fragment {
                         Log.d(TAG, "DocumentSnapshot data: " + document.getData());
                         txtProfileName.setText(name);
                         txtDescription.setText(description);
+                        /*
                         recipeList = (List<String>) document.getData().get("recipeNames");
                         mRecyclerViewRecipes = rootView.findViewById(R.id.recipeList);
                         mLayoutManagerRecipes = new LinearLayoutManager(getActivity());
@@ -84,7 +95,7 @@ public class ProfileActivity extends Fragment {
                         mRecyclerViewRecipes.setLayoutManager(mLayoutManagerRecipes);
                         AdapterRecipes = new recipesAdapter(recipeList, mContext);
                         mRecyclerViewRecipes.setAdapter(AdapterRecipes);
-
+                           */
                     }
                 }
             }
@@ -100,14 +111,60 @@ public class ProfileActivity extends Fragment {
             return rootView;
         txtProfileName = rootView.findViewById(R.id.nameTxt);
         txtDescription = rootView.findViewById(R.id.descriptionTxt);
+        btnDescr = rootView.findViewById(R.id.descrBtn);
+        btnDescr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editDescription(v);
+            }
+        });
 
+        btnSignUp = rootView.findViewById(R.id.signout);
+        btnSignUp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                SignOut(v);
+            }
+        });
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         return rootView;
 
+    }
 
+    public void editDescription (View view){
+        String nDescription;
+        nDescription = txtDescription.getText().toString();
+        DocumentReference docRef = db.collection("profiles").document(id);
+        docRef.update("description", nDescription)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Snapshot successfully updated");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error updating document",e);
+                    }
+                });
 
     }
+
+    public void SignOut(View view){
+        AuthUI.getInstance()
+                .signOut(getContext())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(getContext(), LoginActivity.class);
+                        startActivity(intent);
+                    }
+                });
+    }
+
+
 /*
     public void onViewCreated(View view, Bundle saveInstanceState){
         txtProfileName.setText("HI");
